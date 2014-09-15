e70eb9f9c960deeb17ac7da59abf34963431440c
Janusz Jablonski <januszjablonski.pl@gmail.com>
2014-09-16 00:31:35 +0200

    Test comparing.

---
+++ test3/4.js
@@ -0,0 +1,14 @@
+"use strict";
+
+var x = 5000000000;
+//      #___---___---
+
+var value = [];
+var result;
+
+while(x--) {
+//    result = Boolean(value);
+//    result = !!value;
+//    result = true == !!value;
+    result = true == value;
+}
