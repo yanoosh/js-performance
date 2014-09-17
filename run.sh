b415d3a6111bfc33c847bf1ab7f37a957723de98
Janusz Jablonski <januszjablonski.pl@gmail.com>
2014-09-17 23:03:45 +0200

    Added printing script before test.

--- run.sh
+++ run.sh
@@ -3,6 +3,8 @@
 for script in $@
 do
     echo ${script}
+    cat ${script}
+    echo ""
     date >> ${script}.result
     time -f 't:%U m:%MKB' -a -o ${script}.result node ${script}; tail -n1 ${script}.result
     time -f 't:%U m:%MKB' -a -o ${script}.result node ${script}; tail -n1 ${script}.result
